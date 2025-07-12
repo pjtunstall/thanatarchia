109674d13324a70424f04ab9c8c664bd2206b492
pjtunstall <pjtunstall@gmail.com>
2025-07-12 11:02:47 +0100

    Style return to home button on page not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -12,21 +12,24 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <p className="text-xl text-gray-600 mb-4 uncial">
+        <p className="text-xl text-gray-400 mb-4 uncial">
           Vae mihi! Paginam non invenit
         </p>
-        <p className="text-xl text-gray-600 mb-4">
+        <p className="text-xl text-red-600 mb-4">
           Οὐαί μοι! Ἡ σελίς οὐχ εὑρέθη
         </p>
-        <p className="text-xl text-gray-600 mb-4">
+        <p className="text-xl text-yellow-600 mb-4">
           𐍅𐌰𐌹 𐌼𐌹𐍃! 𐍃𐍄𐌰𐌸 𐌽𐌹 𐌱𐌹𐌲𐌹𐍄𐌰𐌽𐍃 𐍅𐌰𐍂𐌸
         </p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+        <a
+          href="/"
+          className="inline-block px-4 py-2 text-lg font-serif text-yellow-100 bg-amber-800 border-2 border-amber-600 rounded-xl shadow-md hover:bg-amber-700 hover:text-white transition duration-150"
+        >
+          ⮐ Return to Home
         </a>
       </div>
     </div>
