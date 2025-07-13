6852206f28149f76cb50f9e3a8bbfe961816e1a7
pjtunstall <pjtunstall@gmail.com>
2025-07-13 16:48:21 +0100

    WIP: leader bios

--- src/hooks/gameState/initializeLeaders.ts
+++ src/hooks/gameState/initializeLeaders.ts
@@ -1,4 +1,5 @@
 import { Faction, CharacterPortrait } from "@/types/GameTypes";
+import { sub } from "date-fns";
 
 export function initializeLeaders(factions: Faction[]): CharacterPortrait[] {
   return factions.map((faction) => {
@@ -48,7 +49,6 @@ const epithets = [
   "Quirky",
   "Rascal",
   "Rude",
-  "Sly",
   "Terrible",
   "Tyrant",
   "Unkind",
@@ -59,26 +59,50 @@ const epithets = [
 ];
 
 function writeBiography(leader: CharacterPortrait, faction: Faction): string {
-  let title;
+  let title: string;
   if (faction.type === "imperial") {
     title = leader.gender === "male" ? "Emperor" : "Empress";
   } else {
     title = leader.gender === "male" ? "King" : "Queen";
   }
 
+  const scapegoat = scapegoats[Math.floor(Math.random() * scapegoats.length)];
   const action = actions[Math.floor(Math.random() * actions.length)];
   const relative = relatives[Math.floor(Math.random() * relatives.length)];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const weapon = weapons[Math.floor(Math.random() * weapons.length)];
   const place = places[Math.floor(Math.random() * places.length)];
 
-  const possessive = leader.gender === "male" ? "his" : "her";
+  const benefactor =
+    benefactors[Math.floor(Math.random() * benefactors.length)];
+  const victim1 = victims1[Math.floor(Math.random() * victims1.length)];
+  const victim2 = victims2[Math.floor(Math.random() * victims2.length)];
 
-  return `${leader.name}, was unanimously elected ${title}, of the ${faction.name}, by the grace of God, after ${action} ${possessive} ${relative} with a ${adjective} ${weapon} in ${place}.`;
+  let subjectPronoun: string;
+  let possessivePronoun: string;
+
+  if (leader.gender === "male") {
+    subjectPronoun = "He";
+    possessivePronoun = "his";
+  } else {
+    subjectPronoun = "She";
+    possessivePronoun = "her";
+  }
+
+  return `${leader.name} was unanimously elected ${title} of the ${
+    faction.name
+  }, by the grace of God, after ${scapegoat} tragically ${action} ${possessivePronoun} ${relative} with a ${adjective} ${weapon} in ${place}. (Particularly harrowing as ${subjectPronoun.toLowerCase()} was the only witness.) ${subjectPronoun} is known for ${possessivePronoun} patronage of ${benefactor}, and ${possessivePronoun} persecution of ${victim1}, ${victim2}--and you if you don't play your cards right.`;
 }
 
-const actions = ["bludgeoning", "strangling", "stabbing", "garrotting"];
-const adjectives = ["blunt", "damp", "golden"];
+const scapegoats = [
+  "unbelievers",
+  "witches etc.",
+  "enemies of the state",
+  "political rivals",
+  "a mystery assassin",
+];
+const actions = ["bludgeoned", "strangled", "stabbed", "garrotted"];
+const adjectives = ["blunt", "damp", "golden", "rusty"];
 const relatives = [
   "father",
   "mother",
@@ -88,9 +112,24 @@ const relatives = [
   "twin neices",
   "twin nephews",
 ];
-const weapons = ["table cloth", "candlestick", "Septuagint"];
+const weapons = [
+  "table cloth",
+  "candlestick",
+  "Septuagint",
+  "hauberk",
+  "trenchard",
+  "palfrey",
+];
 const places = [
   "the bath",
   "the royal bedchamber",
   "the Basilica of Saint Jerome",
 ];
+
+const benefactors = [
+  "honest, hard-working noblefolk",
+  "major landowners",
+  "wealthy supporters",
+];
+const victims1 = ["beekeepers", "under fives", "plague victims"];
+const victims2 = ["the infirm", "the elderly", "the poor"];
