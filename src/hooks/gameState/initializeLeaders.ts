8f222608a40c9298d2d1c421640533afb143a1c0
pjunstall <pjtunstall@gmail.com>
2025-07-13 13:17:54 +0100

    WIP: leader bios

--- src/hooks/gameState/initializeLeaders.ts
+++ src/hooks/gameState/initializeLeaders.ts
@@ -1,16 +1,22 @@
 import { Faction, CharacterPortrait } from "@/types/GameTypes";
 
-export const initializeLeaders = (factions: Faction[]): CharacterPortrait[] => {
+export function initializeLeaders(factions: Faction[]): CharacterPortrait[] {
   return factions.map((faction) => {
     const leader =
       Math.random() < 0.5 ? faction.leader.male : faction.leader.female;
     if (faction.name !== "Romans") {
+      const prefix =
+        Math.random() < 0.1 && faction.faith !== "Pagan" ? "Saint " : "";
       const epithet = epithets[Math.floor(Math.random() * epithets.length)];
-      leader.name = `${leader.name} the ${epithet}`;
+      leader.name = `${prefix + leader.name} the ${epithet}`;
     }
+
+    const biography = writeBiography(leader, faction);
+    console.log(biography);
+
     return leader;
   });
-};
+}
 
 const epithets = [
   "Apostate",
@@ -21,6 +27,7 @@ const epithets = [
   "Cruel",
   "Dire",
   "Embezzler",
+  "Entitled",
   "Faithless",
   "False",
   "Fierce",
@@ -36,7 +43,7 @@ const epithets = [
   "Notorious",
   "Oath-Breaker",
   "Petulant",
-  "Pious",
+  "'Pious'",
   "Proud",
   "Quirky",
   "Rascal",
@@ -49,5 +56,41 @@ const epithets = [
   "Unsteady",
   "Vicious",
   "Vile",
-  "Wry",
+];
+
+function writeBiography(leader: CharacterPortrait, faction: Faction): string {
+  let title;
+  if (faction.type === "imperial") {
+    title = leader.gender === "male" ? "Emperor" : "Empress";
+  } else {
+    title = leader.gender === "male" ? "King" : "Queen";
+  }
+
+  const action = actions[Math.floor(Math.random() * actions.length)];
+  const relative = relatives[Math.floor(Math.random() * relatives.length)];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const weapon = weapons[Math.floor(Math.random() * weapons.length)];
+  const place = places[Math.floor(Math.random() * places.length)];
+
+  const possessive = leader.gender === "male" ? "his" : "her";
+
+  return `${leader.name}, was unanimously elected ${title}, of the ${faction.name}, by the grace of God, after ${action} ${possessive} ${relative} with a ${adjective} ${weapon} in ${place}.`;
+}
+
+const actions = ["bludgeoning", "strangling", "stabbing", "garrotting"];
+const adjectives = ["blunt", "damp", "golden"];
+const relatives = [
+  "father",
+  "mother",
+  "sister",
+  "brother",
+  "cousin",
+  "twin neices",
+  "twin nephews",
+];
+const weapons = ["table cloth", "candlestick", "quill", "Septuagint"];
+const places = [
+  "the bath",
+  "the royal bedchamber",
+  "the Basilica of Saint Jerome",
 ];
